e5b2edeab53888d152b72cb65ecd42e9ca1b047a
REEVE <nreevevi6512@gmail.com>
2020-08-16 15:05:59 +0800

    save

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const bcrypt = require("bcrypt");
+const passport = require("passport");
+const flash = require("express-flash");
+const session = require("express-session");
+
+const initializePassport = require("./passport-config");
+initializePassport(passport, (email) => {
+  return users.find((user) => user.email === email);
+});
 
 const users = [];
 
 app.set("view-engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //route
 app.get("/", (req, res) => {
@@ -17,6 +40,15 @@ app.get("/", (req, res) => {
 app.get("/login", (req, res) => {
   res.render("login.ejs");
 });
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
 //REGISTER
 app.get("/register", (req, res) => {
